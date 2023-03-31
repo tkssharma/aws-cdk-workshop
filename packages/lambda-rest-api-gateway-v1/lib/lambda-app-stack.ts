d39448ee1b8d4e9b0fac3d3c34e2dd7ace985d0d
tkssharma <tarun.softengg@gmail.com>
2023-03-31 16:21:51 +0530

    feat(api gateway apps added): api gateway demo apps added

---
+++ packages/lambda-rest-api-gateway-v1/lib/lambda-app-stack.ts
@@ -0,0 +1,64 @@
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const api = new apigateway.RestApi(this, 'api', {
+      description: 'example api gateway',
+      deployOptions: {
+        stageName: 'dev',
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowCredentials: true,
+        allowOrigins: ['http://localhost:3000'],
+      },
+    });
+
+    // 👇 create an Output for the API URL
+    new cdk.CfnOutput(this, 'apiUrl', { value: api.url });
+
+    // 👇 define get todos function
+    const getTodosLambda = new lambda.Function(this, 'get-todos-lambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+    });
+
+    // 👇 add a /todos resource
+    const todos = api.root.addResource('todos');
+
+    // 👇 integrate GET /todos with getTodosLambda
+    todos.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getTodosLambda, { proxy: true })
+    );
+
+    // 👇 define delete todo function
+    const deleteTodoLambda = new lambda.Function(this, 'delete-todo-lambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+    });
+
+    // 👇 add a /todos/{todoId} resource
+    const todo = todos.addResource('{todoId}');
+
+    // 👇 integrate DELETE /todos/{todoId} with deleteTodosLambda
+    todo.addMethod(
+      'DELETE',
+      new apigateway.LambdaIntegration(deleteTodoLambda)
+    );
+  }
+}
---
+++ packages/lambda-rest-api-gateway-v2/lib/lambda-app-stack.ts
@@ -0,0 +1,104 @@
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /*
+        const httpApi = new HttpApi(this, 'cors-demo-api', {
+      description: 'API for CORS demo',
+      corsPreflight: {
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowMethods: [
+          CorsHttpMethod.OPTIONS,
+          CorsHttpMethod.GET,
+          CorsHttpMethod.POST,
+          CorsHttpMethod.PUT,
+          CorsHttpMethod.PATCH,
+          CorsHttpMethod.DELETE,
+        ],
+        allowCredentials: true,
+        allowOrigins: ['http://localhost:3000'],
+        // 👇 optionally cache responses to preflight requests
+        // maxAge: cdk.Duration.minutes(5),
+      },
+    });
+
+    */
+    // 👇 create our HTTP Api
+    const httpApi = new HttpApi(this, 'http-api-example', {
+      description: 'HTTP API example',
+      corsPreflight: {
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowMethods: [
+          CorsHttpMethod.OPTIONS,
+          CorsHttpMethod.GET,
+          CorsHttpMethod.POST,
+          CorsHttpMethod.PUT,
+          CorsHttpMethod.PATCH,
+          CorsHttpMethod.DELETE,
+        ],
+        allowCredentials: true,
+        allowOrigins: ['http://localhost:3000'],
+      },
+    });
+
+    // 👇 create get-todos Lambda
+    const getTodosLambda = new lambda.Function(this, 'get-todos', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+    });
+
+    // 👇 add route for GET /todos
+    httpApi.addRoutes({
+      path: '/todos',
+      methods: [HttpMethod.GET],
+      integration: new HttpLambdaIntegration(
+        'get-todos-integration',
+        getTodosLambda
+      ),
+    });
+
+    // 👇 create delete-todos Lambda
+    const deleteTodoLambda = new lambda.Function(this, 'delete-todo', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+    });
+
+    // 👇 add route for DELETE /todos/{todoId}
+    httpApi.addRoutes({
+      path: '/todos/{todoId}',
+      methods: [HttpMethod.DELETE],
+      integration: new HttpLambdaIntegration(
+        'delete-todo-integration',
+        deleteTodoLambda
+      ),
+    });
+
+    // 👇 add an Output with the API Url
+    new cdk.CfnOutput(this, 'apiUrl', {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      value: httpApi.url!,
+    });
+  }
+}
